7de8935037f7dbca8416cd198ed9da2a7f150db0
baranacikgoz <baran-acikgoz@outlook.com>
2022-03-31 11:49:04 +0300

    deleted comment lines

--- lib/has_wifi_rtt.dart
+++ lib/has_wifi_rtt.dart
@@ -1,8 +1,3 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -10,6 +5,7 @@ import 'package:flutter/services.dart';
 class HasWifiRtt {
   static const MethodChannel _channel = MethodChannel('has_wifi_rtt');
 
+  /// Returns a Future<bool> which indicates the device has wifi rtt or not.
   static Future<bool> checkRtt() async {
     return await _channel.invokeMethod("checkRtt");
   }
