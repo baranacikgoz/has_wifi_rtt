c341baa42d6aa1490916a44b7a99e1cda2fe2fef
baranacikgoz <baran-acikgoz@outlook.com>
2022-03-31 11:18:11 +0300

    example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:has_wifi_rtt/has_wifi_rtt.dart';
 
 void main() {
@@ -16,33 +15,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _hasRttFeature = "Unknown";
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await HasWifiRtt.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+  Future<void> setRttStatus() async {
+    final bool _answer = await HasWifiRtt.checkRtt();
     setState(() {
-      _platformVersion = platformVersion;
+      _hasRttFeature = _answer.toString();
     });
   }
 
@@ -50,13 +28,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Has Wifi Rtt Example App'),
+          ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Has this device have Wi-Fi RTT support?: $_hasRttFeature"),
+              ElevatedButton(
+                  onPressed: () => setRttStatus(), child: const Text("Check")),
+            ],
+          ))),
     );
   }
 }
