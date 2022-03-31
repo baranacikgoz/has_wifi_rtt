73fb0118c5e29d215c874030bb25443bb8551cc5
baranacikgoz <baran-acikgoz@outlook.com>
2022-03-31 11:45:13 +0300

    tests

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # has_wifi_rtt
 A flutter plugin package to check whether device has Wi-Fi RTT Scan Feature or not.
+
+## Usage
+```
+import 'package:has_wifi_rtt/has_wifi_rtt.dart'
+
+final bool response = await HasWifiRtt.checkRtt();
+```  
+
+Also chech the example app.
--- test/has_wifi_rtt_test.dart
+++ test/has_wifi_rtt_test.dart
@@ -7,17 +7,7 @@ void main() {
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await HasWifiRtt.platformVersion, '42');
+  testWidgets('Returns a Future<bool>', (WidgetTester tester) async {
+    expect(HasWifiRtt.checkRtt() is Future<bool>, true);
   });
 }
